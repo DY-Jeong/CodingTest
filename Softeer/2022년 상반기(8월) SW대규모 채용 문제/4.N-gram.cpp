d1863f2d255b4bdf4f68907a1b47eb9fdc2fbf49
DY-Jeong <202150212@o.cnu.ac.kr>
2023-08-04 21:27:16 +0900

    모비스 22년 상반기(8월)SW채용 문제 업뎃

---
+++ Softeer/2022년 상반기(8월) SW대규모 채용 문제/4.N-gram.cpp
@@ -0,0 +1,18 @@
+// 문제 : 
+// Bigram은 2글자씩 떼어낸 문자열을 이야기한다. ababcdefsddsd 라는 문자열이 있을 때 ab, ba, ad, dc, cd, de, ef, fs...이다. 이 때 N-gram의 개수가 많은 것을, 앞글자를 abc순대로 출력하세요.
+
+// 예시)
+// 입력 abcdef
+// 출력
+// ab
+// bc
+// ce
+// de
+// ef
+// (각각 1개씩 있으며 abcde순이다.)
+
+// 입력 abababcefsba
+// 출력
+// ab
+// ba
+// (해설) ab, ba 3개씩 있으며 ab, ba,순이다.
